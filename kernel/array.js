d56c73a8d87588ded288e5a9a19e6b9741fc01fa
W.S. Hager <wshager@gmail.com>
2013-05-07 14:12:31 +0200

    moved apply and merge to array removed merge updated on to use defer and apply

---
+++ kernel/array.js
@@ -0,0 +1,68 @@
+define(["dojo/_base/lang", "dojo/_base/array"],
+	function(lang, array){
+	
+	"use strict";
+
+	var kernel = lang.getObject("acuna.kernel", true);
+
+	kernel.array = {};
+	lang.mixin(kernel.array, {
+		merge: function(stack,args,context) {
+			return [stack,args];
+		},
+		rmerge: function(stack,args,context) {
+			return [args,stack];
+		},
+		prepend: function(stack,args,context) {
+			return array.map(stack[0],function(_){
+				return [_].concat(stack[1]);
+			});
+		},
+		append: function(stack,args,context) {
+			return array.map(stack[0],function(_){
+				return stack[1].concat([_]);
+			});
+		},
+		mapleft: function(stack,args,context){
+			return array.map(stack[0],function(_){
+				return [_,stack[1]];
+			});
+		},
+		mapright: function(stack,args,context){
+			return array.map(stack[0],function(_){
+				return [stack[1],_];
+			});
+		},
+		reverse: function(stack,args,context) {
+			return stack.reverse();
+		},
+		apply: function(stack,args,context){
+			array.forEach(args,function(arg){
+				array.forEach(stack,function(_){
+					arg.apply(null,_);
+				});
+			});
+		},
+		defer:function(stack,args,context) {
+			var replaceArgs = function(args){
+				if(typeof args[0]=="function") {
+					return function(){
+						args[0](stack,args[1],context);
+					};
+				} else {
+					//stack = [args[0]];
+					for(var i=0;i<args.length;i++) {
+						args[i] = args[i] instanceof Array ? replaceArgs(args[i]) : args[i];
+					}
+					return args;
+				}
+			};
+			return stack.map(function(_){
+				return [_].concat(replaceArgs(args));
+			});
+		}
+	});
+	
+	return kernel.array;
+
+});
--- sequence/on.js
+++ sequence/on.js
@@ -1,37 +1,15 @@
-define(["dojo/_base/lang", "dojo/_base/array", "dojo/on", "acuna/sequence/merge", "acuna/sequence/each"],
-	function(lang, array, on, merge, each){
+define(["dojo/_base/lang", "acuna/kernel/array", "dojo/on"],
+	function(lang, array, on){
 	
 	"use strict";
 
 	var seq = lang.getObject("acuna.sequence", true);
 	
-	var defer = function(stack,args,context) {
-		console.log("defer",stack)
-		return function(){
-			args[0](stack,args[1],context);
-		};
-	}
-	
 	seq.on = function(stack,args,context) {
-		stack = merge(stack,args,context);
-		stack = each(stack,[on],context);
+		stack = array.defer(stack,args,context);
+		stack = array.apply(stack,[on],context);
 		return stack;
 	}
-	
-	/*seq.on = function(stack,args,context){
-		// mylistener is function, eg style
-		// style expects list, style array and context
-		var eventName = args[0].toString();
-		var mylistener = args[1];
-		stack = array.map(stack,function(item){
-			var listener = function(e){
-				mylistener[0]([item],mylistener[1],context);
-			};
-			return on(item.domNode, eventName, listener);;
-		});
-		context.stack = stack;
-		return stack;
-	};*/
 	
 	return seq.on;
 	
