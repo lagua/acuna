df3753edecfacf5f9c8327c5d7ae1eccce13ac5f
W.S. Hager <wshager@gmail.com>
2013-05-09 11:22:45 +0200

    removed each from sequence

--- sequence.js
+++ sequence.js
@@ -1,18 +1,16 @@
 define([
 	"dojo/_base/lang",
-	"acuna/sequence/each",
 	"acuna/sequence/filter",
 	"acuna/sequence/mixin",
 	"acuna/sequence/on"
 ],
-	function(lang, each, filter,mixin,on){
+	function(lang, filter,mixin,on){
 	
 	"use strict";
 
 	var seq = lang.getObject("acuna.sequence", true);
 	
 	seq = lang.mixin(seq,{
-		each:each,
 		filter:filter,
 		mixin:mixin,
 		on:on
