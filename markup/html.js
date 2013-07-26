f6d7158c0befc804120221ffb76d2ab189573eba
W.S. Hager <wshager@gmail.com>
2013-07-27 00:27:58 +0200

    place at body by default parse mixed content properly

--- markup/html.js
+++ markup/html.js
@@ -7,6 +7,17 @@ define(["dojo/_base/lang", "dojo/dom-construct"],
 	
 	var traverse = function(name,obj,parent){
 		var attrs = {};
+		if(obj instanceof Array) {
+			var ar = [];
+			obj.forEach(function(_){
+				_ = typeof _ == "object" ? _ : {"#text":_};
+				ar.push(traverse(name,_,parent));
+			});
+			ar.forEach(function(_,i){
+				if(i>0) domConstruct.place(ar[i].firstChild,ar[0]);
+			});
+			return ar[0];
+		}
 		for(var k in obj) {
 			if(k.charAt(0)=="@") {
 				var attr = k.replace("@","");
@@ -16,17 +27,20 @@ define(["dojo/_base/lang", "dojo/dom-construct"],
 		}
 		var elm = domConstruct.create(name,attrs,parent);
 		for(var k in obj) {
-		 if(k=="#text") {
-				elm.appendChild(document.createTextNode(obj[k]));
+			var v = obj[k];
+			if(k=="#text") {
+				elm.appendChild(document.createTextNode(v));
 			} else {
-			traverse(k,obj[k],elm);
+				if(typeof v=="string") v = {"#text":v};
+				traverse(k,v,elm);
 			}
 		}
 		return elm;
 	}
 	
 	markup.html = function(stack,args,context) {
-		stack.push(traverse("body",args[0]).firstCild);
+		stack.push(traverse("body",args));
+		domConstruct.place(stack[0],document.body,"replace");
 		return stack;
 	}
 	
