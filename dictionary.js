e0702466a26636d19ff7ed8d6cf806475895a25e
W.S. Hager <wshager@gmail.com>
2013-08-01 14:23:12 +0200

    finished dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,8 +1,22 @@
-define(["dojo/_base/lang"],function(lang) {
+define(["dojo/_base/lang","dojo/_base/array","dojo/Deferred"],function(lang,array,Deferred) {
 	var acuna = lang.getObject("acuna",true);
 	var dictionary = function(vocab,args){
+		var d = new Deferred();
+		require(args,function(){
+			for(var i=0;i<arguments.length;i++) {
+				var dict = arguments[i];
+				for(var k in vocab) {
+					if(dict[k]) {
+						array.forEach(dict[k],function(_){
+							vocab[_] = vocab[k];
+						});
+					}
+				}
+			}
+			d.resolve(vocab);
+		});
 		console.log(vocab,args);
-		return vocab;
+		return d;
 	};
 	acuna.dictionary = dictionary;
 	return dictionary;
