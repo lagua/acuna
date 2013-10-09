0dc8abca63d46240502ff21a950af0ae3e85bb68
W.S. Hager <wshager@gmail.com>
2013-10-09 17:18:10 +0200

    fixed style/create (single style object)

--- style/create.js
+++ style/create.js
@@ -5,17 +5,33 @@ define([ "dojo/_base/lang","dojo/json" ], function(lang,JSON) {
 		var style = context.document.documentElement.appendChild(context.document.createElement("style"));
 	    style.sheet.insertRule(a, 0);
 	};
-	
+	var find = function(a,context) {
+		var ss = document.styleSheets;
+		for(var i = ss.length - 1; i >= 0; i--) {
+			try {
+				var s = ss[i], rs = s.cssRules ? s.cssRules : s.rules ? s.rules : [];
+				for(var j = rs.length - 1; j >= 0; j--) {
+					if((rs[j].type === context.window.CSSRule.WEBKIT_KEYFRAMES_RULE || rs[j].type === context.window.CSSRule.MOZ_KEYFRAMES_RULE) && rs[j].name == a) {
+						return rs[j];
+					}
+				}
+			} catch (e) {
+			}
+		}
+		return null;
+	};
 	style.create = function(stack,args,context){
 		var x = stack.pop();
-		var step = "{\n";
-		while (x instanceof Object) {
-			for(var k in x) {
-				step += k+":"+x[k]+";\n";
+		var r = args.shift();
+		var step = r + " ";
+		step += JSON.stringify(x,function(key, val){
+			if(key) {
+				return val + ";";
 			}
-			x = stack.pop();
-		}
-		insert(x+" "+step+"}",context);
+			return val;
+		},2).replace(/\"|,/g,"")+"\n";
+		var rule = find(r, context);
+		if(!rule) insert(step, context);
 		return stack;
 	};
 	
