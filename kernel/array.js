81cb2ed4e3ef82699671e310cd31f25a2ead0ab0
wshager <wshager@gmail.com>
2013-10-29 13:49:29 +0100

    array fixes

--- kernel/array.js
+++ kernel/array.js
@@ -5,72 +5,40 @@ define(["dojo/_base/lang", "dojo/_base/array"],
 
 	var kernel = lang.getObject("acuna.kernel", true);
 
-	kernel.array = {};
-	lang.mixin(kernel.array, {
+	lang.mixin(kernel, {
 		forEach: function(stack,args,context){
-			var f = args.shift();
-			array.forEach(stack.pop(),function(_){
-				f.apply(null,[[_]]);
-			});
-			return stack.concat(args.splice(0,args.length));
-		},
-		merge: function(stack,args,context) {
-			return [stack,args];
-		},
-		rmerge: function(stack,args,context) {
-			return [args,stack];
-		},
-		prepend: function(stack,args,context) {
-			return array.map(stack[0],function(_){
-				return [_].concat(stack[1]);
-			});
-		},
-		append: function(stack,args,context) {
-			return array.map(stack[0],function(_){
-				return stack[1].concat([_]);
-			});
-		},
-		mapleft: function(stack,args,context){
-			return array.map(stack[0],function(_){
-				return [_,stack[1]];
-			});
-		},
-		mapright: function(stack,args,context){
-			return array.map(stack[0],function(_){
-				return [stack[1],_];
-			});
-		},
-		reverse: function(stack,args,context) {
-			return stack.reverse();
-		},
-		apply: function(stack,args,context){
-			array.forEach(args,function(arg){
-				array.forEach(stack,function(_){
-					arg.apply(null,_);
-				});
+			var f = stack.pop();
+			var a = stack.pop();
+			var lstack = stack.slice();
+			array.forEach(a,function(_){
+				lstack = f(lstack.concat(_),[],context);
 			});
 			return stack;
 		},
-		defer:function(stack,args,context) {
-			var replaceArgs = function(args){
-				if(typeof args[0]=="function") {
-					return function(){
-						args[0](stack,args[1],context);
-					};
-				} else {
-					//stack = [args[0]];
-					for(var i=0;i<args.length;i++) {
-						args[i] = args[i] instanceof Array ? replaceArgs(args[i]) : args[i];
-					}
-					return args;
-				}
-			};
-			return stack.map(function(_){
-				return [_].concat(replaceArgs(args));
-			});
+		concat: function(stack,args,context) {
+			var x = stack.pop();
+			var y = stack.pop();
+			stack.push(y.concat(x));
+			return stack;
+		},
+		map: function(stack,args,context){
+			var f = stack.pop();
+			var a = stack.pop();
+			var lstack = stack.slice();
+			stack.push(array.map(a,function(_){
+				lstack = f(lstack.concat(_),[],context);
+				return lstack.pop();
+			}));
+			return stack;
+		},
+		reverse: function(stack,args,context) {
+			var x = stack.pop();
+			x.reverse();
+			stack.push(x);
+			return stack;
 		}
 	});
 	
-	return kernel.array;
+	return kernel;
 
 });
